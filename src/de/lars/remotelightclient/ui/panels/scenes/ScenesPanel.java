6c425f5ce650f703fdca8ea6916b09e221e2220b
Drumber <lobrath4@gmail.com>
2019-12-29 16:47:12 +0100

    clean up

--- src/de/lars/remotelightclient/ui/panels/scenes/ScenesPanel.java
+++ src/de/lars/remotelightclient/ui/panels/scenes/ScenesPanel.java
@@ -27,8 +27,6 @@ import javax.swing.ScrollPaneConstants;
 import de.lars.remotelightclient.Main;
 import de.lars.remotelightclient.scene.Scene;
 import de.lars.remotelightclient.scene.SceneManager;
-import de.lars.remotelightclient.settings.SettingsManager;
-import de.lars.remotelightclient.settings.types.SettingObject;
 import de.lars.remotelightclient.ui.MainFrame;
 import de.lars.remotelightclient.ui.MenuPanel;
 import de.lars.remotelightclient.ui.Style;
@@ -44,7 +42,6 @@ public class ScenesPanel extends MenuPanel {
 	private static final long serialVersionUID = 4955214837008536168L;
 	private MainFrame mainFrame;
 	private SceneManager scm = Main.getInstance().getSceneManager();
-	private SettingsManager sm = Main.getInstance().getSettingsManager();
 	private JPanel bgrScenes;
 	private JPanel bgrSettings;
 
