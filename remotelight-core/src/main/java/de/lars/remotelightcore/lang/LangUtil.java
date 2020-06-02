db687ce97722f2b72c3eef6ffd84584824514f57
Drumber <lobrath4@gmail.com>
2020-06-02 18:45:00 +0200

    added license headers

--- remotelight-core/src/main/java/de/lars/remotelightcore/lang/LangUtil.java
+++ remotelight-core/src/main/java/de/lars/remotelightcore/lang/LangUtil.java
@@ -1,17 +1,25 @@
-/*******************************************************************************
- * ______                     _       _     _       _     _   
- * | ___ \                   | |     | |   (_)     | |   | |  
- * | |_/ /___ _ __ ___   ___ | |_ ___| |    _  __ _| |__ | |_ 
- * |    // _ \ '_ ` _ \ / _ \| __/ _ \ |   | |/ _` | '_ \| __|
- * | |\ \  __/ | | | | | (_) | ||  __/ |___| | (_| | | | | |_ 
- * \_| \_\___|_| |_| |_|\___/ \__\___\_____/_|\__, |_| |_|\__|
- *                                             __/ |          
- *                                            |___/           
- * 
- * Copyright (C) 2019 Lars O.
- * 
- * This file is part of RemoteLight.
- ******************************************************************************/
+/*-
+ * >===license-start
+ * RemoteLight
+ * ===
+ * Copyright (C) 2019 - 2020 Drumber
+ * ===
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * <===license-end
+ */
+
 package de.lars.remotelightcore.lang;
 
 import java.util.ArrayList;
