271bb3e0a84b911e800d59752cd96ee0e3cc2550
Drumber <lobrath4@gmail.com>
2020-06-02 18:50:53 +0200

    updated license headers

--- remotelight-core/src/main/java/de/lars/remotelightcore/animation/animations/Meteor.java
+++ remotelight-core/src/main/java/de/lars/remotelightcore/animation/animations/Meteor.java
@@ -2,7 +2,7 @@
  * >===license-start
  * RemoteLight
  * ===
- * Copyright (C) 2019 - 2020 Drumber
+ * Copyright (C) 2019 - 2020 Lars O.
  * ===
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
