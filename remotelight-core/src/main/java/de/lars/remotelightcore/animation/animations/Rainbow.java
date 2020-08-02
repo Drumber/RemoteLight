490028d9ec25d2aa3d5aba6817b7a8d7cf7882f9
Lars <drumberhd@gmail.com>
2020-08-02 13:36:37 +0200

    updated rainbow animations and added some new modes to it > best result on low speed (20ms)

--- remotelight-core/src/main/java/de/lars/remotelightcore/animation/animations/Rainbow.java
+++ remotelight-core/src/main/java/de/lars/remotelightcore/animation/animations/Rainbow.java
@@ -22,54 +22,119 @@
 
 package de.lars.remotelightcore.animation.animations;
 
+import java.awt.Color;
+import java.util.Random;
+
 import de.lars.remotelightcore.RemoteLightCore;
 import de.lars.remotelightcore.animation.Animation;
 import de.lars.remotelightcore.out.OutputManager;
 import de.lars.remotelightcore.settings.SettingsManager.SettingCategory;
-import de.lars.remotelightcore.settings.types.SettingBoolean;
+import de.lars.remotelightcore.settings.types.SettingInt;
+import de.lars.remotelightcore.settings.types.SettingSelection;
+import de.lars.remotelightcore.settings.types.SettingSelection.Model;
 import de.lars.remotelightcore.utils.color.PixelColorUtils;
-import de.lars.remotelightcore.utils.color.RainbowWheel;
 
 public class Rainbow extends Animation {
 	
-	private int step;
+	private final String[] modes = {"Simple - Cycle", "Simple - Static", "Natural - Sinewave", "Natural - Sinebow"};
+	private SettingSelection settingMode;
+	private SettingInt settingResolution;
+	
+	private Color[] strip;
+	private float step;
+	private float increment = 0.001f;
 
 	public Rainbow() {
 		super("Rainbow");
-		this.addSetting(new SettingBoolean("animation.rainbow.cycle", "Cycle", SettingCategory.Intern, null, true));
+		settingMode = this.addSetting(new SettingSelection("animation.rainbow.mode", "Mode", SettingCategory.Intern, "Rainbow mode", modes, modes[0], Model.ComboBox));
+		settingResolution = this.addSetting(new SettingInt("animation.rainbow.resolution", "Resolution", SettingCategory.Intern, "Rainbow resolution", 10, 1, 200, 5));
 	}
 	
 	@Override
 	public void onEnable() {
-		step = 0;
-		if(((SettingBoolean) getSetting("animation.rainbow.cycle")).getValue()) {
-			for(int i = 0; i < RemoteLightCore.getLedNum(); i++) {
-				PixelColorUtils.shiftRight(1);
-				step += 3;
-				if(step >= RainbowWheel.getRainbow().length) {
-					step = 0;
-				}
-				PixelColorUtils.setPixel(0, RainbowWheel.getRainbow()[step]);
-			}
-		}
+		step = new Random().nextFloat(); // random start point
+		strip = PixelColorUtils.colorAllPixels(Color.BLACK, RemoteLightCore.getLedNum());
+		executeSelectedMode(); // fill strip
 		super.onEnable();
 	}
 	
 	@Override
 	public void onLoop() {
-		if(step >= RainbowWheel.getRainbow().length) {
-			step = 0;
-		}
-		
-		if(((SettingBoolean) getSetting("animation.rainbow.cycle")).getValue()) {
-			PixelColorUtils.shiftRight(1);
-			PixelColorUtils.setPixel(0, RainbowWheel.getRainbow()[step]);
-			step += 3;
-		} else {
-			OutputManager.addToOutput(PixelColorUtils.colorAllPixels(RainbowWheel.getRainbow()[step], RemoteLightCore.getLedNum()));
-			step++;
-		}
+		increment = settingResolution.getValue() * 0.001f;
+		executeSelectedMode();
+		OutputManager.addToOutput(strip);
 		super.onLoop();
+	}
+	
+	private void executeSelectedMode() {
+		int modeIndx = settingMode.getSelectedIndex();
+		switch (modeIndx) {
+		case 0:
+			hsvRainbow();
+			break;
+		case 1:
+			hsvNoCycleRainbow();
+			break;
+		case 2:
+			sineWaveRainbow();
+			break;
+		case 3:
+			sinebowRainbow();
+			break;
+		default:
+			hsvRainbow();
+			break;
+		}
+	}
+	
+	private void hsvRainbow() {
+		for(int i = strip.length-1; i > 0; i--) {
+			strip[i] = strip[i-1];
+		}
+		if((step += increment) > 1.0f) {
+			step = 0.0f;
+		}
+		strip[0] = Color.getHSBColor(step, 1.0f, 1.0f);
+	}
+	
+	private void hsvNoCycleRainbow() {
+		Color color = Color.getHSBColor(step, 1.0f, 1.0f);
+		for(int i = 0; i < strip.length; i++) {
+			strip[i] = color;
+		}
+		if((step += increment) > 1.0f) {
+			step = 0.0f;
+		}
+	}
+	
+	private void sineWaveRainbow() {
+		float frequency = increment * 10.0f;
+		for(int i = 0; i < strip.length; i++) {
+			int red	  = (int) (Math.sin(frequency * i + 0 - step) * 127 + 128);
+			int green = (int) (Math.sin(frequency * i + 2 - step) * 127 + 128);
+			int blue  = (int) (Math.sin(frequency * i + 4 - step) * 127 + 128);
+			strip[i] = new Color(red, green, blue);
+		}
+		if((step += 0.1f) > (Float.MAX_VALUE - 2*increment)) {
+			step = 0.0f;
+		}
+	}
+	
+	private void sinebowRainbow() {
+		for(int i = 0; i < strip.length; i++) {
+			double res = 2.0 + increment;
+			double r = (Math.sin(res * Math.PI * (i +       0) - step) * 0.5 + 0.5);
+			double g = (Math.sin(res * Math.PI * (i + 1.0/3.0) - step) * 0.5 + 0.5);
+			double b = (Math.sin(res * Math.PI * (i + 2.0/3.0) - step) * 0.5 + 0.5);
+			
+			int red   = (int) (r * 255.0);
+			int green = (int) (g * 255.0);
+			int blue  = (int) (b * 255.0);
+			strip[i] = new Color(red, green, blue);
+		}
+		if((step += 0.1f) > (Float.MAX_VALUE - 1.0f)) {
+			step = 0.0f;
+		}
 	}
 
 }
