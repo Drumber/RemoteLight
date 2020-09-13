61c844ecf41481bb65df31142aa6ff9185a0c8a4
Lars <drumberhd@gmail.com>
2020-09-13 12:00:50 +0200

    added missing license headers and excluded downloaded java class files

--- remotelight-client/src/main/java/de/lars/remotelightclient/utils/ui/WrapLayout.java
+++ remotelight-client/src/main/java/de/lars/remotelightclient/utils/ui/WrapLayout.java
@@ -1,28 +1,11 @@
-/*-
- * >===license-start
- * RemoteLight
- * ===
- * Copyright (C) 2019 - 2020 Lars O.
- * ===
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * <===license-end
- */
-
 package de.lars.remotelightclient.utils.ui;
 
-import java.awt.*;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Insets;
+
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 
