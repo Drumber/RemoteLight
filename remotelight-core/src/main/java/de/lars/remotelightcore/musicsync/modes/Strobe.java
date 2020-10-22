c16b80c0e4c28854b99cab228412345bd5e31cf4
Lars <drumberhd@gmail.com>
2020-10-22 16:44:29 +0200

    updated Strobe music effect

--- remotelight-core/src/main/java/de/lars/remotelightcore/musicsync/modes/Strobe.java
+++ remotelight-core/src/main/java/de/lars/remotelightcore/musicsync/modes/Strobe.java
@@ -22,25 +22,20 @@
 
 package de.lars.remotelightcore.musicsync.modes;
 
-import de.lars.remotelightcore.utils.color.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.Timer;
-
 import de.lars.remotelightcore.RemoteLightCore;
 import de.lars.remotelightcore.musicsync.MusicEffect;
 import de.lars.remotelightcore.out.OutputManager;
 import de.lars.remotelightcore.settings.SettingsManager.SettingCategory;
 import de.lars.remotelightcore.settings.types.SettingColor;
 import de.lars.remotelightcore.settings.types.SettingInt;
+import de.lars.remotelightcore.utils.color.Color;
 import de.lars.remotelightcore.utils.color.PixelColorUtils;
 import de.lars.remotelightcore.utils.maths.TimeUtil;
 
 public class Strobe extends MusicEffect {
 	
-	private Timer timer;
-	private TimeUtil timeUtil;
+	private TimeUtil timeFlashes;
+	private TimeUtil timeDuration;
 	private boolean triggered;
 	private int flashRatio = 4;
 	private int flashDuration;
@@ -49,26 +44,23 @@ public class Strobe extends MusicEffect {
 
 	public Strobe() {
 		super("Strobe");
-		this.addSetting(new SettingInt("musicsync.strobe.flashratio", "Flash ratio (per second)", SettingCategory.MusicEffect, "Flashes per second", 4, 0, 100, 1));
-		this.addSetting(new SettingInt("musicsync.strobe.flashduration", "Flash duration (in ms)", SettingCategory.MusicEffect, "", 1500, 100, 50000, 100));
+		flashRatio = this.addSetting(new SettingInt("musicsync.strobe.flashratio", "Flash ratio (per second)", SettingCategory.MusicEffect, "Flashes per second", 4, 0, 100, 1)).get();
+		flashDuration = this.addSetting(new SettingInt("musicsync.strobe.flashduration", "Flash duration (in ms)", SettingCategory.MusicEffect, "", 1500, 100, 50000, 100)).get();
 		this.addSetting(new SettingColor("musicsync.strobe.color", "Color", SettingCategory.MusicEffect, "", Color.WHITE));
 		
-		flashRatio = ((SettingInt) getSetting("musicsync.strobe.flashratio")).get();
-		flashDuration = ((SettingInt) getSetting("musicsync.strobe.flashduration")).get();
-		timer = new Timer(1000 / flashRatio, flashListener);
-		timeUtil = new TimeUtil(flashDuration);
+		timeDuration = new TimeUtil(flashDuration, false);
+		timeFlashes = new TimeUtil(1000 / flashRatio, true);
 	}
 	
 	@Override
 	public void onEnable() {
 		triggered = false;
-		timer.stop();
+		flashSwitch = true;
 		super.onEnable();
 	}
 	
 	@Override
 	public void onDisable() {
-		timer.stop();
 		super.onDisable();
 	}
 	
@@ -77,34 +69,30 @@ public class Strobe extends MusicEffect {
 		color = ((SettingColor) getSetting("musicsync.strobe.color")).get();
 		flashRatio = ((SettingInt) getSetting("musicsync.strobe.flashratio")).get();
 		flashDuration = ((SettingInt) getSetting("musicsync.strobe.flashduration")).get();
-		timer.setDelay(1000 / flashRatio);
-		timeUtil.setInterval(flashDuration);
+		timeFlashes.setInterval(1000 / flashRatio);
+		timeDuration.setInterval(flashDuration);
 		
-		if(triggered && timeUtil.hasReached()) {
+		if(triggered && timeDuration.hasReached()) {
 			triggered = false;
-			timer.stop();
 			OutputManager.addToOutput(PixelColorUtils.colorAllPixels(Color.BLACK, RemoteLightCore.getLedNum()));
 		}
 		
-		if(this.isBump() && !triggered) {
+		if(this.isBump()) {
 			flashSwitch = true;
 			triggered = true;
-			timer.start();
-			timeUtil.reset();
+			timeDuration.reset();
 		}
 		
-		super.onLoop();
-	}
-	
-	private ActionListener flashListener = new ActionListener() {
-		@Override
-		public void actionPerformed(ActionEvent e) {
+		// FLASH
+		if(triggered && (timeFlashes.hasReached() || this.isBump())) {
 			if(flashSwitch)
 				OutputManager.addToOutput(PixelColorUtils.colorAllPixels(color, RemoteLightCore.getLedNum()));
 			else
 				OutputManager.addToOutput(PixelColorUtils.colorAllPixels(Color.BLACK, RemoteLightCore.getLedNum()));
 			flashSwitch = !flashSwitch;
 		}
-	};
+		
+		super.onLoop();
+	}
 
 }
