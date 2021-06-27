56ced27b83d7faee175d1be10f19e58caddf8ddc
Lars <29163322+Drumber@users.noreply.github.com>
2021-06-27 19:36:14 +0200

    added draw led output to sound test frame

--- remotelight-client/src/test/java/de/lars/remotelightclient/test/SoundTest.java
+++ remotelight-client/src/test/java/de/lars/remotelightclient/test/SoundTest.java
@@ -1,8 +1,11 @@
 package de.lars.remotelightclient.test;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
@@ -12,10 +15,12 @@ import javax.swing.Timer;
 
 import de.lars.remotelightclient.Main;
 import de.lars.remotelightcore.musicsync.MusicSyncManager;
+import de.lars.remotelightcore.out.OutputManager;
 
 public class SoundTest {
 	
 	private MusicSyncManager msm;
+	private OutputManager om;
 	
 	private final int bars = 60;
 	
@@ -30,6 +35,7 @@ public class SoundTest {
 	public SoundTest() {
 		Main main = new Main(null, true);
 		msm = main.getCore().getMusicSyncManager();
+		om = main.getCore().getOutputManager();
 		
 		Dimension size = new Dimension(width, height);
 		VisualizerPanel visPanel = new VisualizerPanel();
@@ -56,6 +62,12 @@ public class SoundTest {
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
+			
+			//-----------------------
+			// draw led output
+			//-----------------------
+			drawLedOutput((Graphics2D) g, om.getLastColors());
+			
 			if(msm.getSoundProcessor() == null || !msm.getSoundProcessor().isActive())
 				return;
 			
@@ -134,25 +146,63 @@ public class SoundTest {
 		
 		void drawRawVisualizer(Graphics g, float[] amplitudes, double adjustment) {
 			int barWidth = 1;
+			int bars = width;
 			int parts = amplitudes.length / width;
 			
-			for(int i = 0; i < width; i++) {
+			if(amplitudes.length < width) {
+				barWidth = width / amplitudes.length;
+				bars = amplitudes.length;
+			}
+			
+			for(int i = 0; i < bars; i++) {
 				int x = barWidth * i;
+				float loudness = 0.0f;
 				
 				// bundle amplitudes
-				int start = parts * i;
-				float[] subArr = Arrays.copyOfRange(amplitudes, start, start+parts+1);
-				// get sum of array
-				float sum = 0;
-				for(float f : subArr)
-					sum += f;
+				if(amplitudes.length >= width) {
+					int start = parts * i;
+					float[] subArr = Arrays.copyOfRange(amplitudes, start, start+parts+1);
+					// get sum of array
+					float sum = 0;
+					for(float f : subArr)
+						sum += f;
+					// loudness = average of sub array
+					loudness = sum / subArr.length;
+				} else {
+					loudness = amplitudes[i];
+				}
 				
 				// bar height = average of sub array
-				int barHeight = (int) (sum / subArr.length * 10.0 * adjustment);
+				int barHeight = (int) (loudness * 10.0 * adjustment);
 				barHeight = Math.min(height, barHeight);
 				int y = height - barHeight;
 				g.fillRect(x, y, barWidth, barHeight);
 			}
+		}
+		
+		void drawLedOutput(Graphics2D g, de.lars.remotelightcore.utils.color.Color[] strip) {
+			int length = strip.length;
+			float fraction = width / length;
+			int[] x = new int[length];
+			int[] yRed = new int[length];
+			int[] yGreen = new int[length];
+			int[] yBlue = new int[length];
+			
+			for(int i = 0; i < strip.length; i++) {
+				x[i] = (int) Math.floor(fraction * i);
+				yRed[i] = height - height / 255 * strip[i].getRed();
+				yGreen[i] = height - height / 255 * strip[i].getGreen();
+				yBlue[i] = height - height / 255 * strip[i].getBlue();
+			}
+			Stroke oldStroke = g.getStroke();
+			g.setStroke(new BasicStroke(2));
+			g.setColor(Color.RED);
+			g.drawPolyline(x, yRed, length);
+			g.setColor(Color.GREEN);
+			g.drawPolyline(x, yGreen, length);
+			g.setColor(Color.BLUE);
+			g.drawPolyline(x, yBlue, length);
+			g.setStroke(oldStroke);
 		}
 		
 	}
